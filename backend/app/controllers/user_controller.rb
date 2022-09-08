7b5b0809bc4a631c6ae3f22fc98ae4435177f571
Takumi Ikeda <tkik13201@gmail.com>
2022-09-08 16:04:50 +0900

    fix: fixes related to conflict resolution

--- backend/app/controllers/user_controller.rb
+++ backend/app/controllers/user_controller.rb
@@ -1,13 +1,17 @@
 # frozen_string_literal: true
 
-class UserController < ApplicationController
-  skip_before_action :find_user, only: :create_user
-  def create_user
-    user = User.new(firebase_id: params[:uid], username: params[:username])
-    raise unless user.save
+module Api
+  module V1
+    class UserController < ApplicationController
+      skip_before_action :find_user, only: :create_user
+      def create_user
+        user = User.new(firebase_id: params[:uid], username: params[:username])
+        raise unless user.save
 
-    render json: { user_id: user.id }, status: :ok
-  rescue StandardError
-    render json: { message: 'user creation failed!' }, status: :internal_server_error
+        render json: { user_id: user.id }, status: :ok
+      rescue StandardError
+        render json: { message: 'user creation failed!' }, status: :internal_server_error
+      end
+    end
   end
 end
