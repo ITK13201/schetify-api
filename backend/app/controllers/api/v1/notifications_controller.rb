c696771cf4cf8bc90151ddaaff6c577381d118c7
SEED0228 <51746920+SEED0228@users.noreply.github.com>
2022-09-09 18:15:36 +0900

    Update backend/app/controllers/api/v1/notifications_controller.rb
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- backend/app/controllers/api/v1/notifications_controller.rb
+++ backend/app/controllers/api/v1/notifications_controller.rb
@@ -6,7 +6,7 @@ module Api
       skip_before_action :find_user, only: :index
 
       def index
-        notifications = Notification.includes(event: {:users_events => :user})
+        notifications = Notification.includes(event: { :users_events => :user})
         .select('events.id as event_id, events.name as event_name, notifications.title, notifications.contents, notifications.created_at')
                                     .where(:user => {:firebase_id => params[:uid]})
         render json: notifications
